e5dfa9cc9a0a6ddadb7a5518dab1186ebe8fe70a
Anjitha Mohan <anjitha.mohan.ext@flydubai.com>
2021-07-15 11:53:00 +0400

    added navigation drawer

--- app/src/main/java/com/anjitha/nytimesdemoapp/newslist/view/MainActivity.java
+++ app/src/main/java/com/anjitha/nytimesdemoapp/newslist/view/MainActivity.java
@@ -1,6 +1,9 @@
 package com.anjitha.nytimesdemoapp.newslist.view;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -21,6 +24,7 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
  private  ActivityMainBinding binding;
+ public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,34 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        setNavigationDrawer();
 
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.toolbar_item, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * method for setting up the navigation drawer.
+     */
+    private void setNavigationDrawer() {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, binding.drawerLayout, R.string.nav_open, R.string.nav_close);
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
